d3b394fd6e7c61d37938394d961fa55d7e0f62d8
rik <rikhjsmits@gmail.com>
2026-01-04 20:35:05 +0100

    Added filecontext for linux

--- internal/proc/filecontext_linux.go
+++ internal/proc/filecontext_linux.go
@@ -2,14 +2,79 @@
 
 package proc
 
-import "github.com/pranshuparmar/witr/pkg/model"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/pranshuparmar/witr/pkg/model"
+)
 
 // GetFileContext returns file descriptor and lock info for a process
-// Linux implementation - TODO: implement using /proc/<pid>/fd and /proc/locks
+// Will return nil if the context could not be gathered.
 func GetFileContext(pid int) *model.FileContext {
-	// Linux implementation could:
-	// - Count /proc/<pid>/fd entries for open files
-	// - Read /proc/<pid>/limits for file limit
-	// - Parse /proc/locks for file locks
-	return nil
+	var fileContext model.FileContext
+
+	// Count /proc/<pid>/fd entries for open files.
+	fdFiles, err := os.ReadDir(fmt.Sprintf("/proc/%v/fd", pid))
+	if err != nil {
+		return nil
+	}
+
+	fileContext.OpenFiles = len(fdFiles)
+	fileContext.FileLimit = getFileLimit(pid)
+	fileContext.LockedFiles = getLockedFiles(pid)
+
+	return &fileContext
+}
+
+func getFileLimit(pid int) int {
+	const LINUX_DEFAULT_MAX_OPEN_FILES = 0 // TODO: Find real max.
+
+	// Read /proc/<pid>/limits for file limit
+	data, err := os.ReadFile(fmt.Sprintf("/proc/%v/limits", pid))
+	if err != nil {
+		return LINUX_DEFAULT_MAX_OPEN_FILES
+	}
+
+	dataString := string(data)
+	for line := range strings.Lines(dataString) {
+		if !strings.HasPrefix(line, "Max open files") {
+			continue
+		}
+
+		// Data in format: "Max open files $SOFT_LOCK_NUMBER $HARD_LOCK_NUMBER files"
+		fields := strings.Fields(line)
+		softLimit, err := strconv.Atoi(fields[3])
+		if err != nil {
+			return LINUX_DEFAULT_MAX_OPEN_FILES
+		}
+
+		return softLimit
+	}
+
+	return 0
+}
+
+func getLockedFiles(pid int) []string {
+	var locked []string
+	output, err := exec.Command("lslocks", "-o", "PATH", "-p", strconv.Itoa(pid)).Output()
+	if err != nil {
+		return locked
+	}
+
+	// First line of output is PATH (column name) which is not interesting.
+	skippedFirst := false
+	for fileName := range strings.Lines(string(output)) {
+		if !skippedFirst {
+			skippedFirst = true
+			continue
+		}
+
+		locked = append(locked, strings.TrimSpace(fileName))
+	}
+
+	return locked
 }
